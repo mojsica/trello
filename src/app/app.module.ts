ee6fe43d38f686e7148b755a440e98f2b9ee673a
mojsicaIcentic <aleksandr.mojsic@icentic.com>
2021-12-13 19:42:48 +0100

    deleted list.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
-import { ListComponent } from './components/list/list.component';
 import { CardComponent } from './components/card/card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
@@ -21,7 +19,6 @@ import { NewBoardFormComponent } from './components/new-board-form/new-board-for
   declarations: [
     AppComponent,
     BoardComponent,
-    ListComponent,
     CardComponent,
     SidebarComponent,
     DashboardComponent,
